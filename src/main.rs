b9f6029b479253949e351febe8b1c82e338ea926
Luca Corrieri <luca.corrieri@epita.fr>
2021-09-26 23:21:12 +0200

    feat: add progress indicator for image generation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -17,4 +18,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib)
         }
     }
+
+    eprintln!("\nDone!");
 }
