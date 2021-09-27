08a0933fbfc259047331c479a7d3dbffdf56bedc
Luca Corrieri <luca.corrieri@epita.fr>
2021-09-27 22:13:20 +0200

    feat(vec3): add operator overloadings

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 
-use std::ops::Neg;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 /// A simple three dimensional vector
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vec3 {
     x: f64,
     y: f64,
@@ -47,6 +47,10 @@ impl Vec3 {
         self.length_squared().sqrt()
     }
 
+    pub fn normalized(&self) -> Vec3 {
+        *self / self.length()
+    }
+
     /// Returns the dot product two vectors
     pub fn dot(u: &Vec3, v: &Vec3) -> f64 {
         u.x * v.x + u.y * v.y + u.z * v.z
@@ -76,9 +80,99 @@ impl Neg for Vec3 {
     }
 }
 
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, t: f64) -> Self::Output {
+        Vec3::new(self.x * t, self.y * t, self.z * t)
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, u: Vec3) -> Self::Output {
+        u * self
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, t: f64) -> Self::Output {
+        (1.0 / t) * self
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0 / rhs;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn length_works() {
+        assert_eq!(6.0, Vec3::new(4.0, 4.0, 2.0).length());
+    }
+
+    fn dot_product_works() {
+        assert_eq!(
+            11.0,
+            Vec3::dot(&Vec3::new(1.0, 2.0, 3.0), &Vec3::new(1.0, 2.0, 3.0))
+        );
+    }
 
     #[test]
     fn eq_works() {
@@ -87,11 +181,87 @@ mod test {
 
     #[test]
     fn neq_works() {
-        assert_ne!(Vec3::new(1.0, 2.0, 3.0), Vec3::new(0.0, 0.0, 0.0));
+        assert_ne!(Vec3::new(1.0, 2.0, 3.0), Vec3::zero());
     }
 
     #[test]
     fn negation_works() {
         assert_eq!(Vec3::new(-1.0, 2.0, -3.0), -Vec3::new(1.0, -2.0, 3.0));
+    }
+
+    #[test]
+    fn add_works() {
+        let expected = Vec3::new(2.0, 4.0, 6.0);
+        assert_eq!(
+            expected,
+            Vec3::new(1.0, 2.0, 3.0) + Vec3::new(1.0, 2.0, 3.0)
+        );
+    }
+
+    #[test]
+    fn add_assign_works() {
+        let mut u = Vec3::new(1.0, 0.0, 0.0);
+        u += Vec3::new(0.0, 2.0, 3.0);
+        assert_eq!(Vec3::new(1.0, 2.0, 3.0), u);
+    }
+
+    #[test]
+    fn sub_works() {
+        let expected = Vec3::new(0.0, 0.0, 0.0);
+        assert_eq!(
+            expected,
+            Vec3::new(1.0, 2.0, 3.0) - Vec3::new(1.0, 2.0, 3.0)
+        );
+    }
+
+    #[test]
+    fn sub_assign_works() {
+        let mut u = Vec3::new(1.0, 2.0, 3.0);
+        u -= Vec3::new(1.0, 2.0, 1.0);
+        assert_eq!(Vec3::new(0.0, 0.0, 2.0), u);
+    }
+
+    #[test]
+    fn mul_vec3_vec3_works() {
+        let expected = Vec3::new(1.0, 4.0, 9.0);
+        assert_eq!(
+            expected,
+            Vec3::new(1.0, 2.0, 3.0) * Vec3::new(1.0, 2.0, 3.0)
+        );
+    }
+
+    #[test]
+    fn mul_vec3_f64_works() {
+        let expected = Vec3::new(2.0, 4.0, 6.0);
+        assert_eq!(expected, Vec3::new(1.0, 2.0, 3.0) * 2.0);
+        assert_eq!(expected, 2.0 * Vec3::new(1.0, 2.0, 3.0));
+    }
+
+    #[test]
+    fn mul_assign_vec3_f64_works() {
+        let mut u = Vec3::new(1.0, 2.0, 3.0);
+        u *= 2.0;
+        assert_eq!(Vec3::new(2.0, 4.0, 6.0), u);
+    }
+
+    #[test]
+    fn div_vec3_f64_works() {
+        let expected = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(expected, Vec3::new(2.0, 4.0, 6.0) / 2.0);
+    }
+
+    #[test]
+    fn div_assign_f64_works() {
+        let mut u = Vec3::new(2.0, 4.0, 6.0);
+        u /= 2.0;
+        assert_eq!(Vec3::new(1.0, 2.0, 3.0), u);
+    }
+
+    #[test]
+    fn normalized_works() {
+        assert_eq!(
+            Vec3::new(4.0, 4.0, 2.0).normalized(),
+            Vec3::new(4.0 / 6.0, 4.0 / 6.0, 2.0 / 6.0)
+        );
     }
 }
