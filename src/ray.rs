f4f1a11fa826f2e8b5a0b08585316f15a6d0f057
Luca Corrieri <luca.corrieri@epita.fr>
2021-09-29 00:04:38 +0200

    feat(ray): add public visibility to ray functions

--- src/ray.rs
+++ src/ray.rs
@@ -8,20 +8,20 @@ pub struct Ray {
 
 impl Ray {
     /// Constructs a new Ray with given origin and direction
-    fn new(orig: Point3, dir: Vec3) -> Ray {
+    pub fn new(orig: Point3, dir: Vec3) -> Ray {
         Ray { orig, dir }
     }
 
-    fn origin(&self) -> Point3 {
+    pub fn origin(&self) -> Point3 {
         self.orig
     }
 
-    fn direction(&self) -> Vec3 {
+    pub fn direction(&self) -> Vec3 {
         self.dir
     }
 
     /// Returns a position along the vector, with `t` the distance from the origin
-    fn at(&self, t: f64) -> Point3 {
+    pub fn at(&self, t: f64) -> Point3 {
         self.orig + t * self.dir
     }
 }
