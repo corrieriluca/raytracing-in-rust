3cf0d1d944ac84ccdf17ce007365d47f7f567d1a
Luca Corrieri <luca.corrieri@epita.fr>
2021-09-29 00:18:47 +0200

    feat(raytracer): sending rays into the scene

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,52 @@
 use crate::color::Color;
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
 use std::io::stdout;
 
 mod color;
 mod ray;
 mod vec3;
 
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = r.direction().normalized();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..image_width {
-            let color = Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
-            color
+            let pixel_color = ray_color(r);
+            pixel_color
                 .write(&mut stdout())
                 .expect("An error occurred while writing to standard output");
         }
