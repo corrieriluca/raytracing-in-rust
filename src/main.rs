0d086a4392b3195dfc8d1c77081c1b9c45873d61
Luca Corrieri <luca.corrieri@epita.fr>
2021-09-30 00:09:22 +0200

    feat(raytracer): ray-sphere intersection

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ mod color;
 mod ray;
 mod vec3;
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = Vec3::dot(&r.direction(), &r.direction());
+    let b = 2.0 * Vec3::dot(&oc, &r.direction());
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    return discriminant > 0.0;
+}
+
 fn ray_color(r: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().normalized();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
