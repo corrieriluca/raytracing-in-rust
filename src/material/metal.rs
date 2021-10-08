185d6c8183293cecaf0aa6f5031dc670a161b409
Luca Corrieri <luca.corrieri@epita.fr>
2021-10-08 23:21:40 +0200

    feat(metal): fuzzy reflection

--- src/material/metal.rs
+++ src/material/metal.rs
@@ -6,18 +6,22 @@ use crate::vec3::Vec3;
 
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Metal {
-        Metal { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Metal {
+        Metal { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray_in: &Ray, record: &HitRecord) -> Option<(Ray, Color)> {
         let reflected = Vec3::reflect(ray_in.direction().normalized(), record.normal);
-        let scattered = Ray::new(record.intersection, reflected);
+        let scattered = Ray::new(
+            record.intersection,
+            reflected + self.fuzz * Vec3::random_in_unit_sphere(),
+        );
         let attenuation = self.albedo;
 
         if Vec3::dot(&scattered.direction(), &record.normal) > 0.0 {
